9640e45baa9f24c8798a8d87077d31f1a389ddaa
Keisuke Tanaka <keisuketanaka97@gmail.com>
2022-12-31 21:59:00 +0100

    test

--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { InferGetStaticPropsType } from 'next';
 import { url } from 'src/utils/url';
 
@@ -5,6 +7,7 @@ import { Footer } from 'src/components/triggers/Footer';
 import { BaseHeadline as Headline } from 'src/components/uis/BaseHeadline';
 import { BaseSection as Section } from 'src/components/uis/BaseSection';
 import { MenuType } from 'src/types/menu.type';
+import { useEffect, useState } from 'react';
 
 export const getStaticProps = async () => {
   /* Instagram */
@@ -18,34 +21,61 @@ export const getStaticProps = async () => {
   const igRes = await igData.json();
 
   /* Menu */
-  const menuData = await fetch(url.baseUrl + '/api/menu');
-  const menuRes = await menuData.json();
+  // try {
+  //   const menuData = await fetch(url.baseUrl + '/api/menu');
+  //   const menuRes = await menuData.json();
+  // } catch (err) {
+  //   console.log(err);
+  // }
   return {
-    props: { igPhotos: igRes, menu: menuRes as MenuType }
+    props: {
+      igPhotos: igRes
+      // menu: menuRes as MenuType
+    }
   };
 };
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
-const Menu = ({ igPhotos, menu }: Props) => {
+const Menu = ({
+  igPhotos
+}: //  menu
+Props) => {
+  const [menu, setMenu] = useState<MenuType>([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const menuData = await fetch(url.baseUrl + '/api/menu');
+        console.log('menuData', menuData);
+        const menuRes = await menuData.json();
+        console.log('menuRes', menuRes);
+        setMenu(menuRes);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, []);
+
   return (
     <>
       <Section>
         <Headline className='mb-8 sm:mb-12 lg:mb-20 2xl:mb-20' label='Menü' />
         <div className='grid gap-8 md:grid-cols-2 lg:gap-20'>
-          {menu.map(menuObj => (
-            <div className='bg-lighter-yellow rounded-lg px-5 py-8 shadow-lg' key={menuObj.title}>
-              <h3 className='text-2xl font-bold border-b-2 mb-6 border-text-black'>{menuObj.title}</h3>
-              <ul className='flex flex-col gap-3'>
-                {menuObj.items.map(item => (
-                  <li className='flex justify-between gap-8' key={item.name}>
-                    <h4 className='text-lg'>{item.name}</h4>
-                    <span className='text-lg'>{item.price.toFixed(1)}</span>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
+          {menu.length &&
+            menu.map(menuObj => (
+              <div className='bg-lighter-yellow rounded-lg px-5 py-8 shadow-lg' key={menuObj.title}>
+                <h3 className='text-2xl font-bold border-b-2 mb-6 border-text-black'>{menuObj.title}</h3>
+                <ul className='flex flex-col gap-3'>
+                  {menuObj.items.map(item => (
+                    <li className='flex justify-between gap-8' key={item.name}>
+                      <h4 className='text-lg'>{item.name}</h4>
+                      <span className='text-lg'>{item.price.toFixed(1)}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
         </div>
       </Section>
       <Footer igPhotos={igPhotos} />
