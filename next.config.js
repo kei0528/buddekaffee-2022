a46d0479000d7993d723b0ad9e7a8fc5f8a5b10d
Keisuke Tanaka <keisuketanaka97@gmail.com>
2023-01-12 01:13:48 +0100

    try fi

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['scontent-ham3-1.cdninstagram.com', 'https://buddekaffee.de']
+    domains: ['scontent-ham3-1.cdninstagram.com', 'buddekaffee.de', 'www.buddekaffee.de']
   },
   i18n: {
     locales: ['de'],
